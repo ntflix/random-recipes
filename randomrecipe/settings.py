dbbcb058b56b83a7e429f04bcb8abc48142a1d9d
ntflix <felix@iron59.co.uk>
2026-09-16 16:29:45 +0100

    allowed hosts *

--- randomrecipe/settings.py
+++ randomrecipe/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-tb^6ippn#26i@n6e2q%677$t)7r2$zl^z=*xha^qf%%8dze0@f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
